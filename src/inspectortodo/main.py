0dda672f68724dbd0e453820fa4c86dc0130d2cd
Wolfgang Münder <wolfgang.muender@tngtech.com>
2018-04-27 12:07:47 +0200

    introduce click

--- src/inspectortodo/main.py
+++ src/inspectortodo/main.py
@@ -5,25 +5,27 @@ import logging
 import os
 
 import click
-import click_log
 
 from .find_invalid_todos import find_invalid_todos
 
 
-click_log.basic_config()
-
-
 @click.command()
-@click.argument('ROOT_DIR', required=True, help='Root directory to inspect.')
-@click.argument('TICKET-PATTERN', required=True, help='Pattern for ticket reference in todo.')
-@click.option('--version-pattern', help='Pattern for version reference in todo.')
+@click.argument('ROOT_DIR', required=True)
+@click.argument('TICKET_PATTERN', required=True)
+@click.option('--version-pattern', help='Regular expression for version references in todos.')
 @click.option('--version', help='Current version.')
-@click.option('--versions', nargs='*', help='List of versions allowed in todos. Versions increase from left to right.')
+@click.option('--versions', help='List of versions allowed in todos. Versions are separated by comma and increase from left to right.')
 @click.option('--log-level', default='INFO', help="Set the log level (DEBUG, INFO, WARNING, ERROR, CRITICAL)")
 def main(root_dir, ticket_pattern, version_pattern, version, versions, log_level):
+    """
+    ROOT_DIR is the directory to inspect recursively.
+
+    TICKET_PATTERN is a regular expression for the ticket references in todos.
+    """
 
     logging.basicConfig(level=log_level.upper(), format='[%(levelname)s] %(message)s')
 
     root_dir = os.path.normpath(os.path.abspath(root_dir))
+    versions = versions.split(',') if versions else versions
 
     find_invalid_todos(root_dir, ticket_pattern, version_pattern, version, versions)
