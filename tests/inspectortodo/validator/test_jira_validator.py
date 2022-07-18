cfcf23d49a86d50781fa7cc3536e6fb938c1a0f9
jancaron <jan.caron.@outlook.de>
2022-07-18 08:02:26 +0200

    Add support for issue field filtering
    * Adds two new parameters:
      * issue_filter_field, which determines, which field of the issue (atm mainly Jira) will get checked
      * issue_filter_value, which determines the values that issue_filter_field needs to have for the TODO to be checked for the report.
    * Example use case:
      * set issue_filter_key='reporter' and issue_filter_values='Max Mustermann', to only include TODOs with issues reported by Max Mustermann in the final report
    * Added according tests
    * Update README

--- tests/inspectortodo/validator/test_jira_validator.py
+++ tests/inspectortodo/validator/test_jira_validator.py
@@ -9,8 +9,8 @@ from inspectortodo.validator import JiraValidator
 
 class JiraValidatorForTesting(JiraValidator):
 
-    def __init__(self, issue_pattern, allowed_statuses):
-        super().__init__(issue_pattern, '', '', '', allowed_statuses)
+    def __init__(self, issue_pattern, allowed_statuses, issue_filter_field=None, issue_filter_values=None):
+        super().__init__(issue_pattern, '', '', '', allowed_statuses, issue_filter_field, issue_filter_values)
         self.issues = {}
 
     def _init_jira_client(self, url, username, password):
@@ -23,6 +23,9 @@ class JiraValidatorForTesting(JiraValidator):
 PATTERN = r'SP-\d+'
 VALID_STATUS = 'Valid Status'
 INVALID_STATUS = 'Invalid Status'
+FILTER_FIELD = 'filter_key'
+FITTING_FILTER_VALUE = 'Fitting Filter Value'
+UNFITTING_FILTER_VALUE = 'Unfitting Filter Value'
 
 
 def test_valid_status():
@@ -83,9 +86,77 @@ def test_invalid_status_invalid_parent_status():
     assert todo.is_valid is False
 
 
-def _initialize_issue_with_status(status, parent_id):
+def test_invalid_status_fitting_filter_then_todo_marked_invalid():
+    issue_id = 'SP-4711'
+    issue = _initialize_issue_with_status(INVALID_STATUS, None, filter_key=FILTER_FIELD, filter_value=FITTING_FILTER_VALUE)
+
+    todo = Todo('some path', 23, 'TODO ' + issue_id + ': content containing valid issue reference')
+
+    validator = JiraValidatorForTesting(PATTERN, [VALID_STATUS], issue_filter_field=FILTER_FIELD, issue_filter_values=[FITTING_FILTER_VALUE])
+    validator.issues[issue_id] = issue
+    validator.validate(todo)
+
+    assert todo.is_valid is False
+
+
+def test_invalid_status_unfitting_filter_then_todo_marked_valid():
+    issue_id = 'SP-4711'
+    issue = _initialize_issue_with_status(INVALID_STATUS, None, filter_key=FILTER_FIELD, filter_value=UNFITTING_FILTER_VALUE)
+
+    todo = Todo('some path', 23, 'TODO ' + issue_id + ': content containing valid issue reference')
+
+    validator = JiraValidatorForTesting(PATTERN, [VALID_STATUS], issue_filter_field=FILTER_FIELD, issue_filter_values=[FITTING_FILTER_VALUE])
+    validator.issues[issue_id] = issue
+    validator.validate(todo)
+
+    assert todo.is_valid is True
+
+
+def test_invalid_status_filter_key_not_present_in_issue_then_todo_marked_valid():
+    issue_id = 'SP-4711'
+    issue = _initialize_issue_with_status(INVALID_STATUS, None, filter_key=None, filter_value=None)
+
+    todo = Todo('some path', 23, 'TODO ' + issue_id + ': content containing valid issue reference')
+
+    validator = JiraValidatorForTesting(PATTERN, [VALID_STATUS], issue_filter_field=FILTER_FIELD, issue_filter_values=[FITTING_FILTER_VALUE])
+    validator.issues[issue_id] = issue
+    validator.validate(todo)
+
+    assert todo.is_valid is True
+
+
+def test_invalid_status_filter_key_present_in_issue_but_value_none_then_todo_marked_valid():
+    issue_id = 'SP-4711'
+    issue = _initialize_issue_with_status(INVALID_STATUS, None, filter_key=FILTER_FIELD, filter_value=None)
+
+    todo = Todo('some path', 23, 'TODO ' + issue_id + ': content containing valid issue reference')
+
+    validator = JiraValidatorForTesting(PATTERN, [VALID_STATUS], issue_filter_field=FILTER_FIELD, issue_filter_values=[FITTING_FILTER_VALUE])
+    validator.issues[issue_id] = issue
+    validator.validate(todo)
+
+    assert todo.is_valid is True
+
+
+def test_invalid_status_fitting_filter_in_list_then_todo_marked_invalid():
+    issue_id = 'SP-4711'
+    issue = _initialize_issue_with_status(INVALID_STATUS, None, filter_key=FILTER_FIELD, filter_value=[FITTING_FILTER_VALUE, UNFITTING_FILTER_VALUE])
+
+    todo = Todo('some path', 23, 'TODO ' + issue_id + ': content containing valid issue reference')
+
+    validator = JiraValidatorForTesting(PATTERN, [VALID_STATUS], issue_filter_field=FILTER_FIELD, issue_filter_values=[FITTING_FILTER_VALUE])
+    validator.issues[issue_id] = issue
+    validator.validate(todo)
+
+    assert todo.is_valid is False
+
+
+def _initialize_issue_with_status(status, parent_id, filter_key=None, filter_value=None):
     issue = Mock()
     issue.fields = Mock()
     issue.fields.status = status
     issue.fields.parent = parent_id
+    issue.raw = {'fields': {}}
+    if filter_key:
+        issue.raw['fields'] = {filter_key: filter_value}
     return issue
